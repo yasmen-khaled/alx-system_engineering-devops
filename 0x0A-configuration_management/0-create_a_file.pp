b58a1f79dfe1b43905ede8477c8ebfde82bd4c0a
yasmen <yasmen.khaled2000@gmail.com>
2024-01-19 16:39:02 +0200

    done

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,9 +1,6 @@
-# Creates a file
+# creats a file
 file { '/tmp/school':
-  ensure  => file,
-  path    => '/tmp/school',
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet'
-}
+	mode 	=> '0744',
+	owner 	=> 'www-data',
+	group 	=> 'wwww-data',
+	content => 'I love Puppet'
