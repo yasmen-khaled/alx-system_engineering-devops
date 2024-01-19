94f67d3228ea940ece4da136400c7d0320cfc7de
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2024-01-19 20:35:10 +0300

    Up

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,16 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
-package { 'flask' :
-    ensure   => '2.1.0',
-    provider => 'pip3',
-    }
+# Install flex
+package { 'python3-pip':
+ensure => installed,
+}
+
+exec { 'install_flask':
+command => 'pip3 install Flask==2.1.0',
+path    => ['/usr/local/bin', '/usr/bin'],
+require => Package['python3-pip'],
+}
+
+exec { 'install_werkzeug':
+command => 'pip3 install Werkzeug==2.2.1',
+path    => ['/usr/local/bin', '/usr/bin'],
+require => Package['python3-pip'],
+}
