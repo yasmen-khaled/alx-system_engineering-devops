f4e1879b357a748f1c3555f0c3f96229d05bb570
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2024-01-19 17:50:03 +0300

    up

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
-#!/usr/bin/pup
-# Install an especific version of flask (2.1.0)
+#!/usr/bin/puppet
+# Install a specific version of flask (2.1.0)
 package { 'flask' :
-    ensure   => '2.1.0',
-    provider => 'pip3',
-    }
+   ensure  => installed,
+   version => '2.1.0',
+   provider => pip3,
+}
