da3995de47ed06edececec73d1ae3f3bedcf4a76
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2024-01-19 18:06:35 +0300

    Update

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,6 +1,9 @@
-# creats a file
+# Creates a file
 file { '/tmp/school':
-	mode 	=> '0744',
-	owner 	=> 'www-data',
-	group 	=> 'wwww-data',
-	content => 'I love Puppet'
+  ensure  => file,
+  path    => '/tmp/school',
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
+}
