003362a2ab38b2fd01429dda70de01f58dc630bd
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2024-02-19 21:53:02 +0300

    oo

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-'''A script that gathers employee name completed
-tasks and total number of tasks from an API
+'''api
 '''
 
 import re
@@ -10,21 +9,21 @@ import sys
 REST_API = "https://jsonplaceholder.typicode.com"
 
 if __name__ == '__main__':
-        if len(sys.argv) > 1:
-                    if re.fullmatch(r'\d+', sys.argv[1]):
-                                    id = int(sys.argv[1])
-                                                emp_req = requests.get('{}/users/{}'.format(REST_API, id)).json()
-                                                            task_req = requests.get('{}/todos'.format(REST_API)).json()
-                                                                        emp_name = emp_req.get('name')
-                                                                                    tasks = list(filter(lambda x: x.get('userId') == id, task_req))
-                                                                                                completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
-                                                                                                            print(
-                                                                                                                                    'Employee {} is done with tasks({}/{}):'.format(
-                                                                                                                                                            emp_name,
-                                                                                                                                                                                len(completed_tasks),
-                                                                                                                                                                                                    len(tasks)
-                                                                                                                                                                                                                    )
-                                                                                                                                                )
-                                                                                                                        if len(completed_tasks) > 0:
-                                                                                                                                            for task in completed_tasks:
-                                                                                                                                                                    print('\t {}'.format(task.get('title')))
+    if len(sys.argv) > 1:
+        if re.fullmatch(r'\d+', sys.argv[1]):
+            id = int(sys.argv[1])
+            emp_req = requests.get('{}/users/{}'.format(REST_API, id)).json()
+            task_req = requests.get('{}/todos'.format(REST_API)).json()
+            emp_name = emp_req.get('name')
+            tasks = list(filter(lambda x: x.get('userId') == id, task_req))
+            completed_tasks = list(filter(lambda x: x.get('completed'), tasks))
+            print(
+                'Employee {} is done with tasks({}/{}):'.format(
+                    emp_name,
+                    len(completed_tasks),
+                    len(tasks)
+                )
+            )
+            if len(completed_tasks) > 0:
+                for task in completed_tasks:
+                    print('\t {}'.format(task.get('title')))
