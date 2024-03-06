c4463b83ca3ea68df4ed60b7cc73f4e9f46db0e5
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2024-03-06 03:57:01 +0300

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return """
+    """Return"""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
