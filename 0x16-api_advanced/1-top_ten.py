2cfd1209b25302217b9cc1ca7073bcda8d735022
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2024-03-06 04:09:21 +0300

    top

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,30 +8,30 @@ import requests
 
 def top_ten(subreddit):
     """Print"""
-   
+    # Construct the URL
     url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
 
-    
+    # Define headers
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
 
-  
+    # Define parameters
     params = {
         "limit": 10
     }
 
-
+    # Send a GET request
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
 
-
+    # Check
     if response.status_code == 404:
         print("None")
         return
 
-
+    # JSON response
     results = response.json().get("data")
 
-
+    # Print
     [print(c.get("data").get("title")) for c in results.get("children")]
