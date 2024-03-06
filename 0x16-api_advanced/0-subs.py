69b21d063aa684f96afa84eedb5fb6b0d8bc7fc9
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2024-03-06 03:40:19 +0300

    subs

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Reddit """
+
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Return total number """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
+        return 0
