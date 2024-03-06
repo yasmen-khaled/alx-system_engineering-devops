b5bf66dfe51d37520b63490393a065efe5a811b0
Yasmen-Abusanoga <63783592+yasmen-khaled@users.noreply.github.com>
2024-03-06 03:53:44 +0300

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ import requests
 def number_of_subscribers(subreddit):
     """Return total number of subscribers for a subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "YourUniqueBot/1.0 (by /u/yourusername)"}
+    headers = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
@@ -15,3 +15,4 @@ def number_of_subscribers(subreddit):
         return subscribers
     else:
         return 0
+        
