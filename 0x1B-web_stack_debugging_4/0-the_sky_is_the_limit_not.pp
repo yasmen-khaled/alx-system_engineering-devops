b569c54900a1420cee2cbd164fbc142211049e1b
yasmen <yasmen.khaled2000@gmail.com>
2024-03-13 22:37:48 +0200

    done

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,5 @@
+# nginx server
+exec { 'fix_limit':
+  path     => ['/usr/bin', '/sbin', '/bin', '/usr/sbin'],
+  command  =>  "sed -i 's/ULIMIT=\"-n 15\"/ULIMIT=\"-n 3072\"/g' /etc/default/nginx; sudo service nginx restart",
+  provider =>  'shell'}
